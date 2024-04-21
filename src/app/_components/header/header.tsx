db0cbc76761f9eed2a7adb63f3516c1fbd89ae32
Web Dev Cody <webdevcody@gmail.com>
2024-04-21 16:05:51 -0400

    update drizzle studio

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -27,7 +27,7 @@ export async function Header() {
     <div className="border-b py-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex gap-8 items-center">
-          <Link href="/" className="flex gap-1 items-center text-xl">
+          <Link href="/" className="flex gap-2 items-center text-xl">
             <Image
               className="rounded w-8 h-8"
               width="50"
@@ -35,7 +35,7 @@ export async function Header() {
               src="/computer.jpeg"
               alt="hero image"
             />
-            TODO Starter
+            TODOStarter
           </Link>
 
           <Links />
